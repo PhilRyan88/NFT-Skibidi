07cc6c9a04614dd6e106b75dafd0139a9e75d3e1
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2024-05-12 21:54:03 +0530

    without _mint function

--- Token.sol
+++ Token.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Skibidi is ERC20 {
     constructor(uint256 initialSupply) ERC20("SkibidiRoshan", "RGAY") {
-        _mint(msg.sender, initialSupply);
+       
     }
 }

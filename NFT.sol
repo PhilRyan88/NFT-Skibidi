b47a80cb5882cca920849fafd1babdaa6ab96521
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2024-05-13 16:13:07 +0530

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -9,7 +9,7 @@ contract GameZ is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Skibidi", "SKBD") {}
+
 
     function awardItem(address fan, string memory tokenURI) public {
         uint256 newItemId = _tokenIds.current();

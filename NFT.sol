e72c8b995fb4796aa6f51d7bb2c601cca3544399
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2024-05-17 22:35:59 +0530

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract GameZ is ERC721URIStorage {
     using Counters for Counters.Counter;

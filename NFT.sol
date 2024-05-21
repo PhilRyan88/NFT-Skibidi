da5fbbff34058ec20836ce0db6c3b24442c3c777
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2024-05-21 06:01:18 +0530

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract GameZ is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
 
     constructor() ERC721("Skibidi", "SKBD") {}
 
